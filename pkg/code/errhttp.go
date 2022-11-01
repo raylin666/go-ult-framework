4c71581694f28fc58ac32c68c646195238b4bce5
linshan <1099013371@qq.com>
2022-11-01 14:31:40 +0800

    fix: errcode format

--- pkg/code/errhttp.go
+++ pkg/code/errhttp.go
@@ -1,23 +1,25 @@
 package code
 
+import "net/http"
+
 /**
 HTTP 状态码设定
 */
 
 var httpCode = map[int]int{
 	/* 系统相关 */
-	ServerError:           500,
-	AuthorizationError:    401,
-	ParamBindError:        400,
-	RequestError:          400,
-	ParamValidateError:    422,
-	UnknownError:          400,
-	DataNotExistError:     400,
-	DataExistError:        400,
-	RequestNotFoundError:  400,
-	DataDeleteError:       400,
-	ResourceNotExistError: 400,
-	DataSelectError:       400,
-	DataCreateError:       400,
-	DataUpdateError:       400,
+	ServerError:           http.StatusInternalServerError,
+	AuthorizationError:    http.StatusUnauthorized,
+	ParamBindError:        http.StatusBadRequest,
+	RequestError:          http.StatusBadRequest,
+	ParamValidateError:    http.StatusUnprocessableEntity,
+	UnknownError:          http.StatusBadRequest,
+	DataNotExistError:     http.StatusBadRequest,
+	DataExistError:        http.StatusBadRequest,
+	RequestNotFoundError:  http.StatusBadRequest,
+	DataDeleteError:       http.StatusBadRequest,
+	ResourceNotExistError: http.StatusBadRequest,
+	DataSelectError:       http.StatusBadRequest,
+	DataCreateError:       http.StatusBadRequest,
+	DataUpdateError:       http.StatusBadRequest,
 }
